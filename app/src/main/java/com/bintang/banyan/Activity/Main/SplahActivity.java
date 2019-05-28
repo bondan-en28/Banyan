601e78292aacf31ab43e7909f0d459135a5ae1b1
daffa200999 <50345397+daffa200999@users.noreply.github.com>
2019-05-28 11:19:37 +0700

    splas activty

---
+++ app/src/main/java/com/bintang/banyan/Activity/Main/SplahActivity.java
@@ -0,0 +1,13 @@
+package com.bintang.banyan.Activity.Main;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplahActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splah);
+    }
+}
